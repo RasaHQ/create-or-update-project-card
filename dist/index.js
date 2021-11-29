f260763cae14e841fc964439ce151a99b5ab0cb3
Yiyao Wei <y.wei@rasa.com>
2021-11-29 21:42:16 +0100

    fix: Unhandled promise rejection (#3)
    * fix
    
    * Update octokit rest api

--- dist/index.js
+++ dist/index.js
@@ -122,7 +122,7 @@ function getProject(projects, projectNumber, projectName) {
 }
 function getColumns(octokit, project) {
     return __awaiter(this, void 0, void 0, function* () {
-        const columns = yield octokit.paginate(octokit.projects.listColumns, {
+        const columns = yield octokit.paginate(octokit.rest.projects.listColumns, {
             project_id: project.id,
             per_page: 100
         });
@@ -256,7 +256,8 @@ function run() {
             }
         }
         catch (error) {
-            if (error.errors.find(e => e.message == 'Project already has the associated issue')) {
+            if (error.errors) {
+              error.errors.find(e => e.message == 'Project already has the associated issue')
                 core.debug("Project already has the associated issue, it's most probably awaiting triage.");
                 core.setOutput('card-id', null);
             }
@@ -787,8 +788,8 @@ class OidcClient {
             const res = yield httpclient
                 .getJson(id_token_url)
                 .catch(error => {
-                throw new Error(`Failed to get ID Token. \n 
-        Error Code : ${error.statusCode}\n 
+                throw new Error(`Failed to get ID Token. \n
+        Error Code : ${error.statusCode}\n
         Error Message: ${error.result.message}`);
             });
             const id_token = (_a = res.result) === null || _a === void 0 ? void 0 : _a.value;
@@ -6580,7 +6581,7 @@ module.exports = require("zlib");
 /************************************************************************/
 /******/ 	// The module cache
 /******/ 	var __webpack_module_cache__ = {};
-/******/ 	
+/******/
 /******/ 	// The require function
 /******/ 	function __nccwpck_require__(moduleId) {
 /******/ 		// Check if module is in cache
@@ -6594,7 +6595,7 @@ module.exports = require("zlib");
 /******/ 			// no module.loaded needed
 /******/ 			exports: {}
 /******/ 		};
-/******/ 	
+/******/
 /******/ 		// Execute the module function
 /******/ 		var threw = true;
 /******/ 		try {
@@ -6603,22 +6604,22 @@ module.exports = require("zlib");
 /******/ 		} finally {
 /******/ 			if(threw) delete __webpack_module_cache__[moduleId];
 /******/ 		}
-/******/ 	
+/******/
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
 /******/ 	}
-/******/ 	
+/******/
 /************************************************************************/
 /******/ 	/* webpack/runtime/compat */
-/******/ 	
+/******/
 /******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
-/******/ 	
+/******/
 /************************************************************************/
-/******/ 	
+/******/
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module is referenced by other modules so it can't be inlined
 /******/ 	var __webpack_exports__ = __nccwpck_require__(109);
 /******/ 	module.exports = __webpack_exports__;
-/******/ 	
+/******/
 /******/ })()
